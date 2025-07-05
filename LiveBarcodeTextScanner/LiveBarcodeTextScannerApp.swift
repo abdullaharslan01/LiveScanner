afd1afdb885b3954cd99eeefa57cc6e0b7bc1759
Abdullah <abdullaharslan1473@gmail.com>
2025-07-05 22:51:13 +0300

    Initial Commit

---
+++ LiveBarcodeTextScanner/LiveBarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveBarcodeTextScannerApp.swift
+//  LiveBarcodeTextScanner
+//
+//  Created by abdullah on 05.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LiveBarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
